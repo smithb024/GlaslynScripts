4794e59e93be1a4d74110b50f89ff5f12069ddc6
smithb024 <bertram.smith@gmail.com>
2024-06-22 23:15:12 +0100

    #7 setup

--- ba.py
+++ ba.py
@@ -1,5 +1,18 @@
 import datetime
 
+# Open the named file and return the contents.
+def OpenFile(filename):
+    contents = []
+    with open(filename) as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=',')
+        lineCount = 0
+        for row in csvReader:
+            content = []
+            contents.append(content)
+            lineCount += 1
+        logFile.write(f'Processed {lineCount} lines.')
+    return contents
+
 print('Start ba')
 
 # Open a log file and add initial entry.
