3d942d3525ce229dd2aa997d6331937fda1646b7
smithb024 <bertram.smith@gmail.com>
2024-06-23 23:23:29 +0100

    #7 Read in data

--- ba.py
+++ ba.py
@@ -22,7 +22,18 @@ def OpenFile(filename):
                     content = []
                     contents.append(content)
                 continue
-
+                
+            # Populate arrays in contents
+            logFile.write(f"Row to analyse: {row}\n")
+            for i in range(0, int(numberOfArrays)):
+                index = i + 1
+                logFile.write(f"Indexes interested in: {index * 2}: {index * 2 + 1}\n")
+                if row[index * 2]:
+                    logFile.write(f"{index} (1) {row[index * 2]}\n")
+                    contents[i].append(row[index * 2])
+                if row[index * 2 + 1]:
+                    logFile.write(f"{index} (2) {row[index * 2 + 1]}\n")
+                    contents[i].append(row[index * 2 + 1])
             lineCount += 1
         logFile.write(f"Processed {lineCount} lines.\n")
     return contents
@@ -40,8 +51,11 @@ logFile.write("Start ba\n")
 stn = OpenFile('stn.csv')
 logFile.write(f"Number of returned cells {len(stn)}.\n")
 
+for row in stn:
+    logFile.write(f"stn row count {len(row)}.\n")
 
-
+for output in stn[0]:
+    logFile.write(f"output: {output}.\n")
 
 
 # Close the log file
