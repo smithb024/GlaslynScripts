e7cf08e0ff3a02fa6503f993085b622fc2e463b8
smithb024 <bertram.smith@gmail.com>
2025-11-27 21:34:27 +0000

    #20 add comments

--- jny.py
+++ jny.py
@@ -1,6 +1,11 @@
 import os
 
 def read_all_lines(folder_path):
+    """
+    Read all the txt files in the folder path. Analyse each line to determine
+    all unique file lines. Sort them and return them.
+    :param folder_path: The path.
+    """
     all_lines = []
 
     for filename in os.listdir(folder_path):
@@ -12,7 +17,6 @@ def read_all_lines(folder_path):
                     route = split_line(line)
                     if exists(all_lines, route) == False:
                         all_lines.append(route)
-                    #all_lines.append(line.rstrip("\n"))
 
     all_lines.sort()
     return all_lines
@@ -29,9 +33,18 @@ def split_line(line):
     return route
 
 def sort_two_strings(a: str, b: str):
+    """
+    Sort two string alphabetically.
+    """
     return tuple(sorted([a, b]))
 
 def exists(all_lines, route):
+    """
+    Go through the all_lines array and see if the route is already present. 
+    Return a flag indicating presence.
+    param all_lines: An array of known routes
+    param route: The route to check
+    """
     for line in all_lines:
         if line[0] == route [0] and line[1] == route [1] and line[2] == route [2]:
             return True
