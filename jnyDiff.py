f4b3d85b0a723c8dc6fe8b183a6103609d28eb59
smithb024 <bertram.smith@gmail.com>
2025-12-08 21:58:56 +0000

    #22: Document

--- jnyDiff.py
+++ jnyDiff.py
@@ -2,6 +2,11 @@ import os
 import sys
 
 def is_present(line, orig_file):
+    """
+    Determine whether line is present in orig_file. Return true if it is.
+    param line The line to test.
+    param orig_file The file to test.
+    """
     with open(orig_file, "r", encoding="utf-8") as f:
         for orig_line in f:
             if orig_line == line:
@@ -10,13 +15,12 @@ def is_present(line, orig_file):
 
 def export_results(all_lines):
     """
-    Save the results in a result fle. Output them as a comma separated file.
+    Save the results in a result file. One line per row in all_lines.
     param all_lines: The lines to output.
     """
     with open(".\\newLines.txt", "w") as file:
         for line in all_lines:
             file.write(line)
-            #file.write("\n")
 
 # Detemine which files to work on.
 orig_file = sys.argv[2]
@@ -26,15 +30,15 @@ new_lines = []
 print(f"original file {orig_file}")
 print(f"new file {new_file}")
 
+# Open new_file and go through each line. Test each line to see if its present in
+# orig_file. If it isn't then output it to the results file.
 with open(new_file, "r", encoding="utf-8") as f:
     line_count = 0
     for line in f:
         line_count += 1
-        #print(f"Line {line_count} {line}")
         present = is_present(line, orig_file)
         if present == False:
             print(f"{line}")
             new_lines.append(line)
 
 export_results(new_lines)
-#print(f"Loaded {line_count} lines from {new_file}")
